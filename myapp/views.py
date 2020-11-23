cca455c7ea377c0e4f17590d8c1e749ef9433524
michitasato <nezimakidorizenmai@gmail.com>
2020-11-24 01:41:16 +0900

    複数ページの移動

--- myapp/views.py
+++ myapp/views.py
@@ -6,5 +6,14 @@ def index(request):
     params = {
         'title': 'Hello/index',
         'msg':'これはサンプルページです',
+        'goto': 'next',
+    }
+    return render(request, 'myapp/index.html', params)
+
+def next(request):
+    params = {
+        'title': 'Hello/Next',
+        'msg':'これはもう１ページです',
+        'goto': 'index',
     }
     return render(request, 'myapp/index.html', params)
