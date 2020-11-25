4298393b80bad280e2360e462e0291eca2c24116
michitasato <nezimakidorizenmai@gmail.com>
2020-11-26 07:05:13 +0900

    Djangoのフォーム機能追加

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('next', views.next, name='next'),
-    path('from', views.form, name='form'),
     path('post_list', views.post_list, name='post_list'),
 ]
 
--- myapp/views.py
+++ myapp/views.py
@@ -3,23 +3,23 @@ from django.http.response import HttpResponse
 from django.utils import timezone
 
 from .models import Post
+from .forms import HelloForm
+
 
 
 def index(request):
     params = {
         'title': 'Hello/index',
-        'msg':'お名前は？',
-        'goto': 'next',
+        'message': 'your data',
+        'form': HelloForm(),
     }
+    if (request.method == 'POST'):
+        params['message'] = '名前：' + request.POST['name'] + \
+        '<br>メール：' + request.POST['mail'] + \
+        '<br>年齢：' + request.POST['age']
+        params['form'] = HelloForm(request.POST)
     return render(request, 'myapp/index.html', params)
 
-def form(request):
-    msg = request.POST['msg']
-    params = {
-        'title': 'Hello/Form',
-        'msg': 'こんにちは' + msg + 'さん。',
-        }
-    return render(request, 'myapp/index.html', params)
 
 def next(request):
     params = {
