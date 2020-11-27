b276d915359b3cf541099e21ec1c70fd654375e5
michitasato <nezimakidorizenmai@gmail.com>
2020-11-27 19:09:50 +0900

    画像を表示

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
+from . import settings
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     path('myapp/', include('myapp.urls')),
     path('admin/', admin.site.urls),
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
