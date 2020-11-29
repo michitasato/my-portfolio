bb3e4265c2bad605a76fa3d7bf274d16bfc6ca5d
michitasato <nezimakidorizenmai@gmail.com>
2020-11-29 17:46:13 +0900

    modal.jsの追加

---
+++ myapp/static/myapp/js/modal.js
@@ -0,0 +1,14 @@
+$(function(){
+
+
+    $('.js-modal-open').on('click',function(){
+        $('.js-modal').fadeIn();
+        return false;
+    });
+
+    
+    $('.js-modal-close').on('click',function(){
+        $('.js-modal').fadeOut();
+        return false;
+    });
+});
--- myapp/static/myapp/js/test.js
+++
@@ -1,4 +0,0 @@
-function ohayou() {
-    alert("Hello!");
-    open('about:blank', '_self').close();    //一度再表示してからClose
-}
