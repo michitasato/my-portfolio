504b324a391440461a9cc0de65b428d9729740fa
michitasato <nezimakidorizenmai@gmail.com>
2020-11-29 19:01:02 +0900

    分岐作成

--- myapp/static/myapp/js/modal.js
+++ myapp/static/myapp/js/modal.js
@@ -1,14 +1,14 @@
 $(function(){
 
-
+   //いいえを選んだ処理
     $('.js-modal-open').on('click',function(){
         $('.js-modal').fadeIn();
         return false;
     });
 
-    
     $('.js-modal-close').on('click',function(){
         $('.js-modal').fadeOut();
         return false;
     });
+
 });
